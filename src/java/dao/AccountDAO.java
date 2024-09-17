47fcc89e1b163edc7500bf323721a96f3f03561b
Phú <phuvdhe176675@fpt.edu.vn>
2024-09-17 21:13:01 +0700

    fe login

---
+++ src/java/dao/AccountDAO.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//package dao;
+//
+//import context.DBContext;
+//import model.Account;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//
+//public class AccountDAO {
+//
+//    Connection conn = null;
+//    PreparedStatement ps = null;
+//    ResultSet rs = null;
+//
+//    public Account login(String username, String password) {
+//        String query = "SELECT * FROM Account WHERE username = ? AND password = ?";
+//        try {
+//            conn = new DBContext().conn;  // Kết nối cơ sở dữ liệu
+//            ps = conn.prepareStatement(query);
+//            ps.setString(1, username);
+//            ps.setString(2, password);
+//            rs = ps.executeQuery();
+//            if (rs.next()) {
+//                return new Account(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
+//                        rs.getString(5), rs.getString(6), rs.getInt(7), rs.getString(8));
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//}
