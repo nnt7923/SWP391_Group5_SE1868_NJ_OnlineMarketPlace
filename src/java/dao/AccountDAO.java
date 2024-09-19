0889cd8a0d376d00ead5f7c7ac823f8b96030131
Phú <phuvdhe176675@fpt.edu.vn>
2024-09-19 17:25:15 +0700

    update database, login

--- src/java/dao/AccountDAO.java
+++ src/java/dao/AccountDAO.java
@@ -1,34 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+
 package dao;
 
 import context.DBContext;
 import model.Account;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AccountDAO {
 
-    Connection conn = null;
-    PreparedStatement ps = null;
-    ResultSet rs = null;
-
     public Account login(String username, String password) {
         String query = "SELECT * FROM Account WHERE username = ? AND password = ?";
-        try {
-            conn = new DBContext().conn;  // Kết nối cơ sở dữ liệu
-            ps = conn.prepareStatement(query);
+        try (Connection conn = new DBContext().conn;
+             PreparedStatement ps = conn.prepareStatement(query)) {
             ps.setString(1, username);
             ps.setString(2, password);
-            rs = ps.executeQuery();
+            ResultSet rs = ps.executeQuery();
             if (rs.next()) {
-                return new Account(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
-                        rs.getString(5), rs.getString(6), rs.getInt(7), rs.getString(8));
+                return new Account(rs.getInt("id"), rs.getString("username"),
+                        rs.getString("password"), rs.getString("email"),
+                        rs.getString("phone"), rs.getString("address"),
+                        rs.getInt("roleID"), rs.getString("status"));
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
